f54510ee2e3b65edb387acade17389a766235b96
skobak <skobak@is2t-intern.com>
2015-09-16 15:10:10 +0200

    WI13620 copyrights

--- ej.widget/src/main/java/ej/widget/Switch.java
+++ ej.widget/src/main/java/ej/widget/Switch.java
@@ -1,5 +1,10 @@
+/*
+ * Java
+ *
+ * Copyright 2015 IS2T. All rights reserved.
+ * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package ej.widget;
-
 
 import ej.microui.display.GraphicsContext;
 import ej.microui.display.shape.AntiAliasedShapes;
--- ej.widget/src/main/java/ej/widget/util/Styles.java
+++ ej.widget/src/main/java/ej/widget/util/Styles.java
@@ -1,3 +1,9 @@
+/*
+ * Java
+ *
+ * Copyright 2015 IS2T. All rights reserved.
+ * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package ej.widget.util;
 
 import ej.components.dependencyinjection.ServiceLoaderFactory;
