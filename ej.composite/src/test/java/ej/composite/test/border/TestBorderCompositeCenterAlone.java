d84e11983e478ac315e24e744b5d56b181cfd2c0
Sébastien Eon <seon@is2t.com>
2015-09-18 09:58:40 +0200

    Add border composite tests

--- ej.composite/src/test/java/ej/composite/test/border/TestBorderCompositeCenterAlone.java
+++ ej.composite/src/test/java/ej/composite/test/border/TestBorderCompositeCenterAlone.java
@@ -9,11 +9,11 @@ package ej.composite.test.border;
 import com.is2t.testsuite.support.CheckHelper;
 
 import ej.composite.BorderComposite;
-import ej.composite.SplitComposite;
 import ej.composite.test.Test;
 import ej.composite.test.TestHelper;
 import ej.composite.widget.Item;
 import ej.microui.MicroUI;
+import ej.microui.display.Display;
 
 /**
  *
@@ -49,27 +49,55 @@ public class TestBorderCompositeCenterAlone extends Test {
 				baseWidth);
 		CheckHelper.check(TestBorderCompositeCenterAlone.class, "h composite height", borderComposite.getHeight(),
 				baseHeight);
+
+		TestHelper.hide(borderComposite);
+		TestHelper.showAndWait(borderComposite, true);
+
+		Display display = Display.getDefaultDisplay();
+		int displayWidth = display.getWidth();
+		int displayHeight = display.getHeight();
+		// get widgets size
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "h center width", centerLabel.getWidth(), displayWidth);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "h center height", centerLabel.getHeight(),
+				displayHeight);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "h composite width", borderComposite.getWidth(),
+				displayWidth);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "h composite height", borderComposite.getHeight(),
+				displayHeight);
 	}
 
 	private static void testVertical() {
 		final int baseWidth = 30;
 		final int baseHeight = 20;
-		float ratio = (float) 1 / 3;
 
-		Item topLabel = new Item(baseWidth, baseHeight);
-		SplitComposite splitComposite = new SplitComposite();
-		splitComposite.setHorizontal(false);
-		splitComposite.setRatio(ratio);
-		splitComposite.add(topLabel);
-		TestHelper.showAndWait(splitComposite, false);
+		Item centerLabel = new Item(baseWidth, baseHeight);
+		BorderComposite borderComposite = new BorderComposite();
+		borderComposite.setHorizontal(false);
+		borderComposite.add(centerLabel);
+		TestHelper.showAndWait(borderComposite, false);
 
 		// get widgets size
-		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v top width", topLabel.getWidth(), baseWidth);
-		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v top height", topLabel.getHeight(), baseHeight);
-		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v composite width", splitComposite.getWidth(),
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v center width", centerLabel.getWidth(), baseWidth);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v center height", centerLabel.getHeight(), baseHeight);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v composite width", borderComposite.getWidth(),
 				baseWidth);
-		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v composite height", splitComposite.getHeight(),
-				baseHeight * 3);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v composite height", borderComposite.getHeight(),
+				baseHeight);
+
+		TestHelper.hide(borderComposite);
+		TestHelper.showAndWait(borderComposite, true);
+
+		Display display = Display.getDefaultDisplay();
+		int displayWidth = display.getWidth();
+		int displayHeight = display.getHeight();
+		// get widgets size
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v center width", centerLabel.getWidth(), displayWidth);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v center height", centerLabel.getHeight(),
+				displayHeight);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v composite width", borderComposite.getWidth(),
+				displayWidth);
+		CheckHelper.check(TestBorderCompositeCenterAlone.class, "v composite height", borderComposite.getHeight(),
+				displayHeight);
 	}
 
 }
---
+++ ej.composite/src/test/java/ej/composite/test/border/TestBorderCompositeNorthAlone.java
@@ -0,0 +1,102 @@
+/*
+ * Java
+ *
+ * Copyright 2015 IS2T. All rights reserved.
+ * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package ej.composite.test.border;
+
+import com.is2t.testsuite.support.CheckHelper;
+
+import ej.composite.BorderComposite;
+import ej.composite.test.Test;
+import ej.composite.test.TestHelper;
+import ej.composite.widget.Item;
+import ej.microui.MicroUI;
+import ej.microui.display.Display;
+import ej.mwt.MWT;
+
+/**
+ *
+ */
+public class TestBorderCompositeNorthAlone extends Test {
+
+	public static void main(String[] args) {
+		MicroUI.start();
+		new TestBorderCompositeNorthAlone().start();
+		MicroUI.stop();
+	}
+
+	@Override
+	protected void run() {
+		testHorizontal();
+		testVertical();
+	}
+
+	private static void testHorizontal() {
+		final int baseWidth = 30;
+		final int baseHeight = 20;
+
+		Item topLabel = new Item(baseWidth, baseHeight);
+		BorderComposite borderComposite = new BorderComposite();
+		borderComposite.setHorizontal(true);
+		borderComposite.add(topLabel, MWT.NORTH);
+		TestHelper.showAndWait(borderComposite, false);
+
+		// get widgets size
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h top width", topLabel.getWidth(), baseWidth);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h top height", topLabel.getHeight(), baseHeight);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h composite width", borderComposite.getWidth(),
+				baseWidth);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h composite height", borderComposite.getHeight(),
+				baseHeight);
+
+		TestHelper.hide(borderComposite);
+		TestHelper.showAndWait(borderComposite, true);
+
+		Display display = Display.getDefaultDisplay();
+		int displayWidth = display.getWidth();
+		int displayHeight = display.getHeight();
+		// get widgets size
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h top width", topLabel.getWidth(), baseWidth);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h top height", topLabel.getHeight(), displayHeight);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h composite width", borderComposite.getWidth(),
+				displayWidth);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h composite height", borderComposite.getHeight(),
+				displayHeight);
+	}
+
+	private static void testVertical() {
+		final int baseWidth = 30;
+		final int baseHeight = 20;
+
+		Item topLabel = new Item(baseWidth, baseHeight);
+		BorderComposite borderComposite = new BorderComposite();
+		borderComposite.setHorizontal(false);
+		borderComposite.add(topLabel, MWT.NORTH);
+		TestHelper.showAndWait(borderComposite, false);
+
+		// get widgets size
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "v top width", topLabel.getWidth(), baseWidth);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "v top height", topLabel.getHeight(), baseHeight);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "v composite width", borderComposite.getWidth(),
+				baseWidth);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "v composite height", borderComposite.getHeight(),
+				baseHeight);
+
+		TestHelper.hide(borderComposite);
+		TestHelper.showAndWait(borderComposite, true);
+
+		Display display = Display.getDefaultDisplay();
+		int displayWidth = display.getWidth();
+		int displayHeight = display.getHeight();
+		// get widgets size
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h top width", topLabel.getWidth(), displayWidth);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h top height", topLabel.getHeight(), baseHeight);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h composite width", borderComposite.getWidth(),
+				displayWidth);
+		CheckHelper.check(TestBorderCompositeNorthAlone.class, "h composite height", borderComposite.getHeight(),
+				displayHeight);
+	}
+
+}
